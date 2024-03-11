35adabb1a1e198c83ee224c678a0c300eab017c3
Barrerajaime <160670377+Barrerajaime@users.noreply.github.com>
2024-03-10 22:00:00 -0500

    Create Venta.java
     se crea un codigo de venta

---
+++ Venta.java
@@ -0,0 +1,14 @@
+public class Venta {
+    private String nombreProducto;
+    private int cantidad;
+    private double totalVenta;
+
+    public Venta(String nombreProducto, int cantidad, double totalVenta) {
+        this.nombreProducto = nombreProducto;
+        this.cantidad = cantidad;
+        this.totalVenta = totalVenta;
+    }
+
+    // Getters y setters
+    // ...
+}
